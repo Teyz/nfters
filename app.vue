843c553b1b02a092ea40766837b8f633b0b895a6
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-04-26 16:00:47 +0200

    feat: seo

--- app.vue
+++ app.vue
@@ -20,6 +20,7 @@
       { name: 'title', content: 'NFTERS - Marketplace' },
       { name: 'description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
       { hid: 'og:title', name: 'og:title', content: 'NFTERS - Marketplace'},
+      { hid: 'og:image', property: 'og:image', content: '/nfters.webp' },
       { name: "msapplication-TileColor", content: "#3D00B7" },
       { name: "theme-color", content: "#ffffff" }
     ],
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    head: {
-        title: 'NFTERS - NFT Marketplace',
-        htmlAttrs: {
-            lang: 'en'
-        },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1' },
-            { hid: 'description', name: 'description', content: 'Junior Software Engineer at Spendesk. Based in Bordeaux. Available for freelance contracts.' },
-            { hid: 'og:title', name: 'og:title', content: 'Bastien Rigaud - Portfolio'},
-            { hid: 'og:image', property: 'og:image', content: './nfters.webp' },
-            { name: "msapplication-TileColor", content: "#da532c" },
-            { name: "theme-color", content: "#ffffff" }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
     vite: {
         css: {
             preprocessorOptions: {
