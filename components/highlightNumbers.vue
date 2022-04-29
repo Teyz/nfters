b5157d3897b3f3001f59a063518a2453e929333b
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-04-29 12:46:17 +0200

    feat: responsive

--- components/highlightNumbers.vue
+++ components/highlightNumbers.vue
@@ -1,50 +1,19 @@
 <template>
     <ul>
         <li>
-            <span class="wRegular integralFont"><h2 id="nbr">32</h2>K+</span>
+            <h2>98<span>K+</span></h2>
             <p>Artwork</p>
         </li>
         <li>
-            <span class="wRegular integralFont"><h2 id="nbr">12</h2>K+</span>
+            <h2>12<span>K+</span></h2>
             <p>Auction</p>
         </li>
         <li>
-            <span class="wRegular integralFont"><h2 id="nbr">15</h2>K+</span>
+            <h2>15<span>K+</span></h2>
             <p>Artist</p>
         </li>
     </ul>
 </template>
-
-<script>
-
-export default defineComponent({
-    setup () {
-        onMounted(() => {
-            incEltNbr();
-        });
-
-        const speed = 30;
-
-        const incEltNbr = () => {
-            const elt = document.querySelectorAll('#nbr');
-            console.log(elt);
-            elt.forEach((el) => {
-                const endNbr = el.innerHTML;
-                incNbrRec(0, endNbr, el);
-            });
-        }
-
-        const incNbrRec = (i, endNbr, elt) => {
-            if (i <= endNbr) {
-                elt.innerHTML = i;
-                setTimeout(function() {
-                incNbrRec(i + 1, endNbr, elt);
-                }, speed);
-            }
-        }
-    }
-});
-</script>
 
 <style scoped lang="scss">
     ul{
@@ -59,11 +28,6 @@ export default defineComponent({
             &:first-child{
                 margin-left: 0;
             }
-        }
-
-        span{
-            font-size: 35px;
-            display: flex;
         }
     }
 </style>
