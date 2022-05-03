fac418539f40e27c40618023683f35329153b555
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-05-03 17:03:40 +0200

    feat: meta

--- app.vue
+++ app.vue
@@ -14,32 +14,6 @@
   </div>
 </template>
 
-<script setup>
-  useHead({
-    viewport: 'width=device-width, initial-scale=1, maximum-scale=5',
-    charset: 'utf-8',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { name: 'title', content: 'NFTERS - Discover, and collect Digital Art NFTs' },
-      { name: 'description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
-      { hid: 'og:title', name: 'og:title', content: 'NFTERS - Discover, and collect Digital Art NFTs'},
-      { hid: 'og:image', property: 'og:image', content: 'https://nfters-marketplace.netlify.app/static/nfters.webp' },
-      { hid: 'twitter:card' , content:"summary_large_image" },
-      { hid: 'twitter:url' , content:"https://nfters-marketplace.netlify.app/" },
-      { hid: 'twitter:title' , content:"NFTERS - Discover, and collect Digital Art NFTs" },
-      { hid: 'twitter:description' , content:"NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT." },
-      { hid: 'twitter:image' , content:"https://nfters-marketplace.netlify.app/static/nfters.webp" },
-      { name: "msapplication-TileColor", content: "#3D00B7" },
-      { name: "theme-color", content: "#ffffff" }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' }
-    ]
-  })
-</script>
-
 <style>
     html,body { margin: 0 !important;}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,21 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/robots'
     ],
+    head: {
+        meta: [
+            { name: 'title', content: 'NFTERS - Discover, and collect Digital Art NFTs' },
+            { name: 'description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
+            { hid: 'og:title', name: 'og:title', content: 'NFTERS - Discover, and collect Digital Art NFTs'},
+            { hid: 'og:image', property: 'og:image', content: 'https://nfters-marketplace.netlify.app/static/nfters.webp' },
+            { hid: 'twitter:card' , content:"summary_large_image" },
+            { hid: 'twitter:url' , content:"https://nfters-marketplace.netlify.app/" },
+            { hid: 'twitter:title' , content:"NFTERS - Discover, and collect Digital Art NFTs" },
+            { hid: 'twitter:description' , content:"NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT." },
+            { hid: 'twitter:image' , content:"https://nfters-marketplace.netlify.app/static/nfters.webp" },
+            { name: "msapplication-TileColor", content: "#3D00B7" },
+            { name: "theme-color", content: "#ffffff" }
+        ]
+    },
     render: {
         bundleRenderer: {
             shouldPreload: (file, type) => {
