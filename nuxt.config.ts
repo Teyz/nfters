d9cbd02f48e5fa845389f37a7d4ef183cea0fd26
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-05-03 17:39:00 +0200

    feat: meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,7 +30,7 @@ export default defineNuxtConfig({
                 { name: "theme-color", content: "#ffffff" }
             ],
             link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+                { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' }
             ]
         }
     },
