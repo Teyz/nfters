f6c59aa1bdca63c893673b8ffa1f1253435afdea
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-05-03 15:09:43 +0200

    feat: meta

--- app.vue
+++ app.vue
@@ -23,15 +23,15 @@
       { name: 'title', content: 'NFTERS - Discover, and collect Digital Art NFTs' },
       { name: 'description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
       { hid: 'og:title', name: 'og:title', content: 'NFTERS - Discover, and collect Digital Art NFTs'},
-      { hid: 'og-image', property: 'og:image',
-        content: 'https://nfters-marketplace.netlify.app/static/nfters.webp'
-      },
+      // { hid: 'og-image', property: 'og:image',
+      //   content: 'https://nfters-marketplace.netlify.app/static/nfters.webp'
+      // },
       { hid: 'og:image', property: 'og:image',
         content: 'https://nfters-marketplace.netlify.app/static/nfters.webp'
       },
-      { hid: 'og-type', property: 'og:type', content: 'website' },
-      { hid: 'og-desc', property: 'og:description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
-      { hid: 'og-url', property: 'og:url', content: 'https://nfters-marketplace.netlify.app/' },
+      // { hid: 'og-type', property: 'og:type', content: 'website' },
+      // { hid: 'og-desc', property: 'og:description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
+      // { hid: 'og-url', property: 'og:url', content: 'https://nfters-marketplace.netlify.app/' },
       { name: "msapplication-TileColor", content: "#3D00B7" },
       { name: "theme-color", content: "#ffffff" }
     ],
