92ff2b75a243b9f8d88d1b6f02a8c58cf3f962df
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-05-03 15:57:38 +0200

    feat: meta

--- app.vue
+++ app.vue
@@ -13,38 +13,19 @@
 </template>
 
 <script setup>
-  useHead({
-    viewport: 'width=device-width, initial-scale=1, maximum-scale=5',
-    charset: 'utf-8',
-    htmlAttrs: {
-      lang: 'en'
-    },
-     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1' },
-      { hid: 'description', name: 'description', content: 'Junior Software Engineer at Spendesk. Based in Bordeaux. Available for freelance contracts.' },
-      { hid: 'og:title', name: 'og:title', content: 'Bastien Rigaud - Portfolio'},
-      { hid: 'og-image', property: 'og:image',
-        content: 'https://www.bastienrigaud.fr/og.webp'
-      },
-      { hid: 'og:image', property: 'og:image',
-        content: 'https://www.bastienrigaud.fr/og.webp'
-      },
-      { hid: 'og-type', property: 'og:type', content: 'website' },
-      { hid: 'og-desc', property: 'og:description', content: 'Junior Software Engineer at Spendesk. Based in Bordeaux. Available for freelance contracts.' },
-      { hid: 'og-url', property: 'og:url', content: 'https://wwww.bastienrigaud.fr' },
-      { name: "msapplication-TileColor", content: "#da532c" },
-      { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 't-type', name: 'twitter:url', content: 'https://wwww.bastienrigaud.fr' },
-      { hid: 't-type', name: 'twitter:title', content: 'Bastien Rigaud - Portfolio' },
-      { hid: 't-type', name: 'twitter:description', content: 'Junior Software Engineer at Spendesk. Based in Bordeaux. Available for freelance contracts' },
-      { hid: 't-type', name: 'twitter:image', content: 'https://www.bastienrigaud.fr/og.webp' },
-      { name: "theme-color", content: "#ffffff" }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  })
+useHead({
+  titleTemplate: 'My App - %s',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  }
+})
 </script>
 
 <style>
