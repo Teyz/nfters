9cd3739a0efd9ce11f5fbd46629d2cd9609bc4f1
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-05-03 15:04:29 +0200

    feat: meta

--- app.vue
+++ app.vue
@@ -12,40 +12,6 @@
   </div>
 </template>
 
-<script setup>
-  useHead({
-    viewport: 'width=device-width, initial-scale=1, maximum-scale=5',
-    charset: 'utf-8',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { name: 'title', content: 'NFTERS - Discover, and collect Digital Art NFTs' },
-      { name: 'description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
-      { hid: 'og:title', name: 'og:title', content: 'NFTERS - Discover, and collect Digital Art NFTs'},
-      { hid: 'og-image', property: 'og:image',
-        content: 'https://nfters-marketplace.netlify.app/static/nfters.webp'
-      },
-      { hid: 'og:image', property: 'og:image',
-        content: 'https://nfters-marketplace.netlify.app/static/nfters.webp'
-      },
-      { hid: 'og-type', property: 'og:type', content: 'website' },
-      { hid: 'og-desc', property: 'og:description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
-      { hid: 'og-url', property: 'og:url', content: 'https://nfters-marketplace.netlify.app/' },
-      { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 't-type', name: 'twitter:url', content: 'https://nfters-marketplace.netlify.app/' },
-      { hid: 't-type', name: 'twitter:title', content: 'NFTERS - Discover, and collect Digital Art NFTs' },
-      { hid: 't-type', name: 'twitter:description', content: 'NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.' },
-      { hid: 't-type', name: 'twitter:image', content: 'https://nfters-marketplace.netlify.app/static/nfters.webp' },
-      { name: "msapplication-TileColor", content: "#3D00B7" },
-      { name: "theme-color", content: "#ffffff" }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' }
-    ]
-  })
-</script>
-
 <style>
     html,body { margin: 0 !important;}
 </style>
