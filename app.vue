662f672c85ddf17b9681d8e03a729fb0d125d698
Bastien Rigaud <bastien.rigaud@spendesk.com>
2022-05-03 16:28:45 +0200

    feat: meta

--- app.vue
+++ app.vue
@@ -3,11 +3,11 @@
     <Head>
       <Title>NFTERS - Marketplace</Title>
       <Meta name="description" content="NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT." />
-      <meta property="og:type" content="website">
-      <meta property="og:url" content="https://nfters-marketplace.netlify.app/">
-      <meta property="og:title" content="NFTERS - Marketplace">
-      <meta property="og:description" content="NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT.">
-      <meta property="og:image" content="https://nfters-marketplace.netlify.app/static/nfters.webp">
+      <Meta property="og:type" content="website" />
+      <Meta property="og:url" content="https://nfters-marketplace.netlify.app/" />
+      <Meta property="og:title" content="NFTERS - Marketplace" />
+      <Meta property="og:description" content="NFTERS is a NFT marketplace powered by Ethereum. Join us and upload / buy your first NFT." />
+      <Meta property="og:image" content="https://nfters-marketplace.netlify.app/static/nfters.webp" />
     </Head>
     <Header/>
     <Hero/>
